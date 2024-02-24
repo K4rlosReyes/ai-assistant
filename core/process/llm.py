88218eb491a27f8e1abda37c24c43c53247eeb64
K4rlosReyes <carlosreyesml18@gmail.com>
2024-02-24 19:20:25 +0100

    feat: make openai the default option

--- core/process/llm.py
+++ core/process/llm.py
@@ -1,4 +1,3 @@
-import os
 from llama_index.llms.llama_cpp import LlamaCPP
 from llama_index.llms.llama_cpp.llama_utils import (
     messages_to_prompt,
@@ -6,20 +5,21 @@ from llama_index.llms.llama_cpp.llama_utils import (
 )
 from llama_index.llms.openai import OpenAI
 from core.manager import settings
-import requests
 
-MODEL = "llama"
+MODEL = "openai"
 
 # LLM selection
 if MODEL == "openai":
     print("USE OPENAI")
     # Use OpenAI model
-    llm = OpenAI(api_key=settings.OPENAI_KEY)
+    system_prompt = """If the user is greeting then respond by saying, "Hello, how may we help you ?"
+                    """
+    llm = OpenAI(model="gpt-4-turbo-preview", api_key=settings.OPENAI_KEY, system_prompt=system_prompt)
 
 else:  # Default to Llama
     print("USE LLAMA")
-    model_url: str = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf"
-    # model_path: str = "core/models/llama-2-13b-chat.Q2_K.gguf"
+    # model_url: str = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf"
+    model_path: str = "core/models/llama-2-13b-chat.Q2_K.gguf"
 
     # TODO: Save the model automatically the first time
     # Check if model is already downloaded
@@ -33,12 +33,12 @@ else:  # Default to Llama
     #     print("Model found.")
 
     llm = LlamaCPP(
-        model_url=model_url,
-        # model_path=model_path,
+        # model_url=model_url,
+        model_path=model_path,
         temperature=0.1,
         max_new_tokens=256,
         context_window=3900,
-        model_kwargs={"n_gpu_layers": 0},  # set GPU layers to 1 if you have one
+        model_kwargs={"n_gpu_layers": 2},  # set GPU layers to 1 if you have one
         verbose=True,
         messages_to_prompt=messages_to_prompt,  # providing additional parameters
         completion_to_prompt=completion_to_prompt,  # providing additional parameters
